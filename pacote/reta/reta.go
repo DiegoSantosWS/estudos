90fe88669190bdf61a8050128251e2b1cf27276d
DiegoSantosWS <tec.infor321@gmail.com>
2017-12-26 17:22:26 -0200

    pacotes

---
+++ pacote/reta/reta.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"math"
+)
+
+// Iniciando com letra maiuscula é PUBLICO (visivel dentro e fora do pacote)!
+// Iniciando com letra minuscula é PRIVADO (visivel apenas dentro do pacore)!
+
+// Por exemplo...
+// Ponto - gerará algo público
+// ponto ou _Ponto - gerará algo privado
+
+// Ponto representa uma coordenada no plano cartesiano
+type Ponto struct {
+	x float64
+	y float64
+}
+
+func catetos(a, b Ponto) (cx, cy float64) {
+	cx = math.Abs(b.x - a.x)
+	cy = math.Abs(b.y - a.y)
+	return
+}
+
+// Distancia é responsavel por calcular a Distancia linear entre dois pontos
+func Distancia(a, b Ponto) float64 {
+	cx, cy := catetos(a, b)
+	return math.Sqrt(math.Pow(cx, 2) + math.Pow(cy, 2))
+}
