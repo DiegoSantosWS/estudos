0a902b5120296dbf48f96bcd2a0b03f520b68d0d
DiegoSantosWS <tec.infor321@gmail.com>
2017-12-29 15:31:43 -0200

    exemplos de testes de funcoes

---
+++ testes/matematica/matematica_test.go
@@ -0,0 +1,14 @@
+package matematica
+
+import "testing"
+
+const erroPadrao = "Valor esperado %v mas o resultado encontrato foi %v."
+
+func TesteMedia(t *testing.T) {
+	valorEperado := 7.28
+	valor := Media(7.2, 9.9, 6.1, 5.9)
+
+	if valor != valorEperado {
+		t.Errorf(erroPadrao, valorEperado, valor)
+	}
+}
