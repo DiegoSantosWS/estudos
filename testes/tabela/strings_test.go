be7b717be8b65ce5a41855a051c9f520755628a9
DiegoSantosWS <tec.infor321@gmail.com>
2017-12-29 15:50:57 -0200

    tabela

--- testes/tabela/strings_test.go
+++ testes/tabela/strings_test.go
@@ -14,7 +14,7 @@ func TestIndex(t *testing.T) {
 		{"Cod3r é show", "Cod3r", 0},
 		{"", "", 0},
 		{"Opa", "opa", -1},
-		{"Leonardo", "o", 2},
+		{"leonardo", "o", 2},
 	}
 
 	for _, teste := range testes {
